009936d5f60f085108286dc66871161a38e39353
mugeishkumar912005 <kmugeis2005@gmail.com>
2025-04-22 14:16:24 +0530

    Task Completed

--- Backend/Dbconfig.js
+++ Backend/Dbconfig.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+    const mongoose = require('mongoose');
+    require('dotenv').config();
 
-const DbConnection = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log("✅ DB Connection Success");
-  } catch (error) {
-    console.error("❌ DB Connection Error:", error);
-  }
-};
-DbConnection();
+    const DbConnection = async () => {
+      try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("✅ DB Connection Success");
+      } catch (error) {
+        console.error("❌ DB Connection Error:", error);
+      }
+    };
+    DbConnection();
