327f52b9a567ca17169501dd99cd90fa6d3257ce
mugeishkumar912005 <kmugeis2005@gmail.com>
2025-04-22 08:27:13 +0530

    Day-2 Completed the 90% of Project✅ Nlp model created connected  stored deployment pending

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';  
 import Nav from './Components/Nav';
-import { Home } from 'lucide-react';
+import  Home from '../src/Components/Home.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
